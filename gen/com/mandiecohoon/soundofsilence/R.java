1efc65ca930bb0af3a1fdfdf962887f4900f3f80
Amanda Cohoon <mandiecohoon@gmail.com>
2015-03-31 10:04:33 -0400

    Added sound
    And started importing sound and play button

--- gen/com/mandiecohoon/soundofsilence/R.java
+++ gen/com/mandiecohoon/soundofsilence/R.java
@@ -18,45 +18,48 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int GridLayout1=0x7f080000;
-        public static final int action_settings=0x7f08000d;
-        public static final int play=0x7f080003;
-        public static final int songProgress=0x7f080002;
-        public static final int songProgressTime=0x7f080001;
-        public static final int toggleButton1=0x7f080007;
-        public static final int toggleButton2=0x7f080008;
-        public static final int toggleButton3=0x7f080009;
-        public static final int toggleButton4=0x7f080004;
-        public static final int toggleButton5=0x7f080006;
-        public static final int toggleButton6=0x7f080005;
-        public static final int toggleButton7=0x7f08000a;
-        public static final int toggleButton8=0x7f08000b;
-        public static final int toggleButton9=0x7f08000c;
+        public static final int GridLayout1=0x7f090000;
+        public static final int action_settings=0x7f09000d;
+        public static final int play=0x7f090003;
+        public static final int songProgress=0x7f090002;
+        public static final int songProgressTime=0x7f090001;
+        public static final int toggleButton1=0x7f090007;
+        public static final int toggleButton2=0x7f090008;
+        public static final int toggleButton3=0x7f090009;
+        public static final int toggleButton4=0x7f090004;
+        public static final int toggleButton5=0x7f090006;
+        public static final int toggleButton6=0x7f090005;
+        public static final int toggleButton7=0x7f09000a;
+        public static final int toggleButton8=0x7f09000b;
+        public static final int toggleButton9=0x7f09000c;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
+    }
+    public static final class raw {
+        public static final int waluigi=0x7f040000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int button_play=0x7f050008;
-        public static final int timer=0x7f050009;
-        public static final int toggle_1=0x7f050002;
-        public static final int toggle_2=0x7f050003;
-        public static final int toggle_3=0x7f050004;
-        public static final int toggle_4=0x7f050005;
-        public static final int toggle_5=0x7f050006;
-        public static final int toggle_6=0x7f050007;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int button_play=0x7f060008;
+        public static final int timer=0x7f060009;
+        public static final int toggle_1=0x7f060002;
+        public static final int toggle_2=0x7f060003;
+        public static final int toggle_3=0x7f060004;
+        public static final int toggle_4=0x7f060005;
+        public static final int toggle_5=0x7f060006;
+        public static final int toggle_6=0x7f060007;
     }
     public static final class style {
         /** 
@@ -80,10 +83,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/mandiecohoon/soundofsilence/MainActivity.java
+++ src/com/mandiecohoon/soundofsilence/MainActivity.java
@@ -1,18 +1,49 @@
 package com.mandiecohoon.soundofsilence;
 
 import android.app.Activity;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.util.SparseIntArray;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	private static final int WAH_SOUND_ID = 0;
+	private static final int CANNON_SOUND_ID = 1;
+	private static final int BLOCKER_SOUND_ID = 2;
+	private SoundPool soundPool;
+	private SparseIntArray soundMap;
+	private Button playButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+		
+		soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+		soundMap = new SparseIntArray(3);
+		soundMap.put(WAH_SOUND_ID, soundPool.load(getBaseContext(), R.raw.waluigi, 1));
+		//soundMap.put(CANNON_SOUND_ID, soundPool.load(context, R.raw.cannon_fire, 1));
+		//soundMap.put(BLOCKER_SOUND_ID, soundPool.load(context, R.raw.blocker_hit, 1));
+		
+		playButton = (Button) findViewById(R.id.play);
+		playButton.setOnClickListener(playButtonListener);
 
+	}
+	
+	 public OnClickListener playButtonListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			
+		}
+	};
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
